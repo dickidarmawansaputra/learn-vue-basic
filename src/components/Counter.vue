797710d9a3f3f0d98ddc720f1a09a1277ee44a7a
Dicki Darmawan Saputra <dickidarmawansaputra@gmail.com>
2025-02-10 21:30:37 +0700

    feat: reactive state using ref

---
+++ src/components/Counter.vue
@@ -0,0 +1,53 @@
+<script setup>
+import { nextTick, ref } from 'vue';
+
+console.info("load component")
+// using reactive state
+let count = ref(0);
+
+function increment() {
+    count.value++;
+    console.info(`Counter: ${count.value}`)
+
+    // sebelum pake reative state, update DOM manual
+    // document.getElementById("count").innerText = `Counter: ${count}`;
+}
+
+// ref bisa nerima semua tipe data
+let countObject = ref({
+    count: 0,
+    name: "Dicki",
+});
+
+function incrementObject() {
+    countObject.value.count++;
+    console.info(`Counter Object: ${countObject.value.count}`)
+}
+
+async function incrementObjectWithAsync() {
+    countObject.value.count++;
+    console.info("increment count before next tick");
+    console.info(`Counter Object: ${countObject.value.count}`)
+
+    countObject.value.count++;
+    countObject.value.count++;
+    countObject.value.count++;
+
+    // pejadwalan vue render ulang (tunggu render ulang)
+    await nextTick();
+    console.info("increment count after next tick");
+}
+</script>
+
+<template>
+    <div>
+        <h1>Counter: {{ count }}</h1>
+        <h1>Counter Object: {{ countObject.count }}</h1>
+        <button v-on:click="increment">Increment</button>
+        <button v-on:click="incrementObject">Increment Object</button>
+        <button v-on:click="incrementObjectWithAsync">Increment Object With Async</button>
+    </div>
+</template>
+
+<style scoped>
+</style>
