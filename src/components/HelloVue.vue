f9f7e03e00ce58e2f30f6a9ff569d1994dd3a799
Dicki Darmawan Saputra <dickidarmawansaputra@gmail.com>
2025-02-10 17:45:00 +0700

    feat: template attributes

--- src/components/HelloVue.vue
+++ src/components/HelloVue.vue
@@ -2,6 +2,14 @@
 console.info("Hello Vue")
 
 const hello = "<b>Hello Vue</b>"
+const classHello = "hello"
+const id = "hello"
+const buttonDisabled = true 
+
+const data = {
+    id: "hello",
+    class: "hello",
+}
 </script>
 
 <template>
@@ -10,6 +18,18 @@ const hello = "<b>Hello Vue</b>"
 
 <!-- jika ingin raw html, bisa menggunakan directive v-html -->
 <div v-html="hello"></div>
+
+<!-- template attributes -->
+<h1 :id v-bind:class="classHello">{{ hello }}</h1>
+<!-- dengan shortcut -->
+<h1 :class="classHello">{{ hello }}</h1>
+
+<!-- v-bind boolean attribute -->
+<button :disabled="buttonDisabled">Button disabled</button>
+
+<!-- multiple attributes -->
+ <h1 v-bind="data">{{ hello }}</h1>
+ <h1 :class="data.class">{{ hello }}</h1>
 </template>
 
 <style scoped>
