fc8af638265c10b4bd87661801971e79b1e73924
Dicki Darmawan Saputra <dickidarmawansaputra@gmail.com>
2025-02-11 13:57:30 +0700

    feat: vue style

---
+++ src/components/Style.vue
@@ -0,0 +1,47 @@
+<script setup>
+import { ref } from 'vue';
+
+const styles = ["red", "bold"];
+
+const red = ref(false);
+const bold = ref(false);
+
+function toggleRed() {
+    red.value = !red.value;
+}
+
+function toggleBold() {
+    bold.value = !bold.value;
+}
+
+const inlineStyleColor = {
+    color: "red",
+}
+
+// karena di js tidak bisa seperti kode css, jadi di css: text-transform => textTransform
+const inlineStyleText ={
+    textTransform: "uppercase",
+    fontWeight: "bold",
+}
+</script>
+<template>
+    <!-- red:red = class red di style : red ref -->
+    <h1 v-bind:class="{red:red, bold:bold }">Style</h1>
+    <h1 v-bind:class="styles">Style</h1>
+
+    <h1 v-bind:style="inlineStyleColor">Inline Style</h1>
+    <h1 v-bind:style="inlineStyleText ">Inline Style</h1>
+    <h1 v-bind:style="[inlineStyleColor, inlineStyleText] ">Inline Style</h1>
+    <button v-on:click="toggleRed">Red</button>
+    <button v-on:click="toggleBold">Bold</button>
+</template>
+<style scoped>
+.red {
+    color: red;
+}
+
+.bold {
+    text-transform: uppercase;
+    font-weight: bold;
+}
+</style>
--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,8 @@ export default defineConfig({
         index: "index.html",
         hello: "hello.html",
         counter: "counter.html",
-        sayhello: "say-hello.html",
+        say_hello: "say-hello.html",
+        style: "style.html",
       }
     }
   }
