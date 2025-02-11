a9786c2ae695f914bf245d8ddd89d211b77f2dd7
Dicki Darmawan Saputra <dickidarmawansaputra@gmail.com>
2025-02-11 10:52:31 +0700

    feat: computed properties

--- src/components/SayHello.vue
+++ src/components/SayHello.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { reactive } from 'vue';
+import { computed, reactive, ref } from 'vue';
 
 // reactive hanya untuk tipe data object, array, colllection (bukan tipe data primitive)
 // cocok untuk kompleks object, tp jika manipulasi yang lebih di kompleks object bisa gunakan kembali ref
@@ -12,14 +12,35 @@ function sayHello() {
     person.firstName = document.getElementById("firstName").value;
     person.lastName = document.getElementById("lastName").value;
 }
+
+// tanpa computed properties
+// function fullName() {
+//     console.info("fullname called");
+//     return `${person.firstName} ${person.lastName}`;
+// }
+
+const count = ref(0);
+
+function increment() {
+    console.info("increment called");
+    count.value++;
+}
+
+// untuk melihat value sebelumnya bisa ditambahkan di parameter
+const fullName = computed((oldName) => {
+    console.info("fullname called");
+    console.info(`old name value: ${oldName}`);
+    return `${person.firstName} ${person.lastName}`;
+});
 </script>
 <template>
     <div>
+        <button v-on:click="increment">Increment: {{ count }}</button>
         <input type="text" id="firstName" placeholder="First Name">
         <input type="text" id="lastName" placeholder="Last Name">
         <button v-on:click="sayHello">Say Hello</button>
     </div>
-    <h1>Hello, {{ person.firstName }} {{ person.lastName }}</h1>
+    <h1>Hello, {{ fullName  }}</h1>
 </template>
 <style scoped>
 </style>
