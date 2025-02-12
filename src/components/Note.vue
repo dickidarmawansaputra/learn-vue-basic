8647c7f4515f2587aa116c4318931f99676f4ebf
Dicki Darmawan Saputra <dickidarmawansaputra@gmail.com>
2025-02-12 15:43:10 +0700

    feat: template refs

---
+++ src/components/Note.vue
@@ -0,0 +1,38 @@
+<script setup>
+import { reactive, ref, useTemplateRef } from 'vue';
+
+const notes = reactive([]);
+const note = ref("");
+const noteInput = useTemplateRef("noteInput");
+const noteList = useTemplateRef("noteList");
+
+function addNote() {
+    if (note.value) {
+        notes.push(note.value);
+        note.value = "";
+        noteInput.value.focus();
+    }
+    
+    // useTemplateRef dalam v-for
+    if (noteList.value) {
+        noteList.value.forEach((li) => {
+            console.info(li.textContent);
+        });
+    }
+}
+</script>
+<template>
+    <h1>Create Note</h1>
+    <div>
+        <input type="text" ref="noteInput" v-model="note" placeholder="Write note here...">
+        <button @click="addNote">Add Note</button>
+    </div>
+
+    <h1>List Note</h1>
+    <ul>
+        // useTemplateRef dalam v-for
+        <li v-for="note in notes" ref="noteList">{{ note }}</li>
+    </ul>
+</template>
+<style scoped>
+</style>
