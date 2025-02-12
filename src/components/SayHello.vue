9facbd68563bf09eb98952ac60a7fb019395c567
Dicki Darmawan Saputra <dickidarmawansaputra@gmail.com>
2025-02-12 10:48:26 +0700

    feat: event handling

--- src/components/SayHello.vue
+++ src/components/SayHello.vue
@@ -32,6 +32,18 @@ const fullName = computed((oldName) => {
     console.info(`old name value: ${oldName}`);
     return `${person.firstName} ${person.lastName}`;
 });
+
+// gunakan parameter event untuk akses event object
+// dibanding document.getElementById("eventFirstName").value
+function changeFirstName(event) {
+    // cara manual event modifier
+    // event.preventDefault()
+    person.firstName = event.target.value;
+}
+
+function changeLastName(event) {
+    person.lastName = event.target.value;
+}
 </script>
 <template>
     <div>
@@ -41,6 +53,18 @@ const fullName = computed((oldName) => {
         <button v-on:click="sayHello">Say Hello</button>
     </div>
     <h1>Hello, {{ fullName  }}</h1>
+
+    <hr>
+    <h1>Vue Event Handling</h1>
+    <div>
+        <form>
+            <button @click="count++">Increment: {{ count }}</button>
+            <!-- @input = v-on:input (shortcut event handler) -->
+            <input type="text" id="eventFirstName" placeholder="First Name" @input="changeFirstName">
+            <input type="text" id="eventLastName" placeholder="Last Name" @input="changeLastName">
+            <button v-on:click.prevent="sayHello">Say Hello</button>
+        </form>
+    </div>
 </template>
 <style scoped>
 </style>
