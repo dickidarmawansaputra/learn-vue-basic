78003f2c220a83b546864dd9492465d1af0053a1
Dicki Darmawan Saputra <dickidarmawansaputra@gmail.com>
2025-02-16 14:39:38 +0700

    feat: fallthrough attributes

---
+++ src/components/ButtonApp.vue
@@ -0,0 +1,24 @@
+<script setup>
+import MyButton from './MyButton.vue';
+
+function clickHandler() {
+    alert("clicked me!");
+}
+</script>
+<template>
+    <!-- semua attribute akan otomatis fallthrough attributes, syaratnya harus singgle element -->
+    <!-- attribute otomatis diteruskan ke element component -->
+    <MyButton class="button" @click="clickHandler" name="Dicki Darmawan Saputra" contoh="vue"></MyButton>
+</template>
+<style scoped>
+.button {
+  background-color: #04AA6D; /* Green */
+  border: none;
+  color: white;
+  padding: 15px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+}
+</style>
---
+++ src/components/MyButton.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { useAttrs } from 'vue';
+
+// untuk menonaktifkan Fallthrough Attributes
+defineOptions({
+    inheritAttrs: false,
+})
+
+// kalo defineProps() maka contoh akan masuk di props, jika tanpa props maka otomatis masuk ke useAttrs()
+// const props = defineProps(["contoh"]);
+// console.info(props);
+
+// cuma disarankan tetap didefinisikan pake props
+const attributes = useAttrs();
+ console.info(attributes);
+</script>
+<template>
+    <button>My Button: {{ attributes.name }}</button>
+</template>
+<style scoped>
+</style>
