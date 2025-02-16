12d0b2bf2437ddfd469efb7678319af54c7c9816
Dicki Darmawan Saputra <dickidarmawansaputra@gmail.com>
2025-02-16 16:01:45 +0700

    feat: component slot

--- src/components/ButtonApp.vue
+++ src/components/ButtonApp.vue
@@ -1,4 +1,5 @@
 <script setup>
+import Container from './Container.vue';
 import MyButton from './MyButton.vue';
 
 function clickHandler() {
@@ -8,7 +9,26 @@ function clickHandler() {
 <template>
     <!-- semua attribute akan otomatis fallthrough attributes, syaratnya harus singgle element -->
     <!-- attribute otomatis diteruskan ke element component -->
-    <MyButton class="button" @click="clickHandler" name="Dicki Darmawan Saputra" contoh="vue"></MyButton>
+    <!-- <MyButton class="button" @click="clickHandler" name="Dicki Darmawan Saputra" contoh="vue"></MyButton> -->
+
+    <!-- Component slot -->
+    <!-- <Container title="Contoh button">
+      <MyButton class="button" @click="clickHandler" name="Dicki Darmawan Saputra" contoh="vue"></MyButton>
+    </Container> -->
+    
+    <!-- named slot -->
+    <Container title="Contoh button">
+      <template v-slot:header="attributes">
+        <h1>Ini header {{ attributes.counter }}</h1>
+      </template>
+      <!-- shortcut v-slot:content -->
+      <template #content>
+        <p>Ini Content</p>
+      </template>
+      <template v-slot:footer>
+        <h4>Ini footer</h4>
+      </template>
+    </Container>
 </template>
 <style scoped>
 .button {
---
+++ src/components/Container.vue
@@ -0,0 +1,44 @@
+<script setup>
+import { ref, useSlots } from 'vue';
+
+const {title} = defineProps({
+    title: String,
+});
+
+// conditional slots
+const slots = useSlots();
+
+const counter = ref(0);
+</script>
+<template>
+    <h1>{{ title }}</h1>
+    <div>
+        <!-- <slot/> -->
+         <!-- default value slot, jika tidak diisi -->
+        <!-- <slot>
+            <p>Default slot</p>
+        </slot> -->
+
+        <!-- named slot -->
+        <div v-if="slots.header">
+            <slot name="header" :counter="counter">
+                <p>Header slot</p>
+            </slot>
+        </div>
+        <div v-if="slots.content">
+            <slot name="content">
+                <p>Content slot</p>
+            </slot>
+        </div>
+        <div v-if="slots.footer">
+            <slot name="footer">
+                <p>Footer slot</p>
+            </slot>
+        </div>
+        <div>
+            <button @click="counter++">Increment Counter</button>
+        </div>
+    </div>
+</template>
+<style scoped>
+</style>
